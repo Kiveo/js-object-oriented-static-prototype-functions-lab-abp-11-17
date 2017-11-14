e2d290347bb280782f1082bbe789a9f9bd0328b5
Maverick Tesini <mavericktesini@gmail.com>
2017-11-14 15:56:45 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,10 +1,12 @@
 function IcebreakerResponse(email){
   this.email = email
 
-  IcebreakerResponse.BatchCreate = function(data){
+
+}
+
+
+IcebreakerResponse.BatchCreate = function(data){
   return data.map(function(ibrData){
     return new IcebreakerResponse(ibrData[0], ibrData[1])
   })
-}
-
 }
